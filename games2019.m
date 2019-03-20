0e5d42609ad5d3f42a35f698868f3935b6932a64
ppxma7 <ppxma7@nottingham.ac.uk>
2019-03-20 09:12:14 +0000

    adding latest

--- games2019.m
+++ games2019.m
@@ -2,13 +2,19 @@
 % 
 % [ma] Feb 2019
 clear variables
-myGames = {'Tetris Effect', 'Far Cry New Dawn', 'Night in the Woods'}';
-whichMonth = {'January', 'February', 'February'}';
-myCost = [34.99 31.99 11.99]';
+myGames = {'Tetris Effect', 'Far Cry New Dawn', 'Night in the Woods', ...
+    'Shadow of the TR', 'ACO:Helix credits', 'RDR2'}';
+whichMonth = {'January', 'February', 'February','February', 'March', ...
+    'March'}';
+
+%check month
+thismonth = str2num(datestr(now,5));
+
+myCost = [34.99 31.99 11.99 34.99 15.99 39.99]';
 
 figure('Position', [100 100 800 400])
 myfontsize = 16;
-g = gramm('x', myGames, 'y', myCost, 'color', whichMonth);
+g = gramm('x', whichMonth, 'y', myCost, 'color', myGames);
 g.geom_point('alpha', 0.8);
 g.set_text_options('font', 'Menlo', 'base_size', myfontsize)
 g.set_names('x',[], 'y', 'Cost (£)','color', 'Month')
@@ -20,4 +26,4 @@ g.set_order_options('x', 0,'color',0)
 g.draw()
 
 fprintf('\nTotal spent this year: £%.2f\n',sum(myCost));
-fprintf('\nMean spend this year: £%.2f\n',mean(myCost));
+fprintf('\nMean monthly spend so far: £%.2f\n',sum(myCost)./thismonth);
